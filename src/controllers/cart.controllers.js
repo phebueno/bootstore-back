1678584e771427e61df83416d994340bab485e10
Phebueno <fcbgomes@gmail.com>
2023-04-27 21:32:39 -0300

    feat: adição de validações para id de produtos e divisão em middlewares

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -1,25 +1,19 @@
 import { ObjectId } from "mongodb";
 import { db } from "../database/database.connection.js";
 
-//Valor placeholder, deve passar pela validação para pegar o valor do usuário:
-const user = {
-  _id: "154654d0zgrzd504zsd5rfernando",
-  name: "fernando",
-};
 //Validar se ID do produto existe?
 
 export async function addCartProduct(req, res) {
   const productId = req.params.productId;
-
+  const session = res.locals.session;
   try {
     const newProduct = await db
       .collection("carts")
       .updateOne(
-        { userId: user._id },
+        { userId: session.userId },
         { $addToSet: { productIdList: productId } },
         { upsert: true }
       );
-    console.log(newProduct);
     if (newProduct.matchedCount === 1 && newProduct.modifiedCount === 0) {
       return res.status(409).send("O item já está presente no seu carrinho!");
     }
@@ -31,23 +25,21 @@ export async function addCartProduct(req, res) {
 
 export async function deleteCartProduct(req, res) {
   const productId = req.params.productId;
-
+  const session = res.locals.session;
   try {
     const product = await db
       .collection("carts")
-      .findOne({ userId: user._id, productIdList: productId });
+      .findOne({ userId: session.userId, productIdList: productId });
     if (!product)
       return res.status(404).send("O item não está no seu carrinho!");
     const delProduct = await db
       .collection("carts")
-      .updateOne({ userId: user._id }, { $pull: { productIdList: productId } });
-
-    console.log(delProduct);
+      .updateOne({ userId: session.userId }, { $pull: { productIdList: productId } });
     if (delProduct.matchedCount === 1 && delProduct.modifiedCount === 0) {
       return res.status(404).send("O item não está presente no seu carrinho!");
     }
     if (product.productIdList.length === 1 && delProduct.matchedCount === 1) {
-      await db.collection("carts").deleteOne({ userId: user._id });
+      await db.collection("carts").deleteOne({ userId: session.userId });
     }
 
     res.send("Item deletado do carrinho!");
@@ -57,17 +49,18 @@ export async function deleteCartProduct(req, res) {
 }
 
 export async function getCart(req, res) {
+    const session = res.locals.session;
   try {
-    const cart = await db.collection("carts").findOne({ userId: user._id });
+    const cart = await db.collection("carts").findOne({ userId: session.userId });
+    if(!cart) return res.send([]);
     const prodListObjectId = cart.productIdList.map((id) => new ObjectId(id));
     //TROCAR COLLECTION PARA PRODUCTS
     const cartProducts = await db
-      .collection("teste")
+      .collection("products")
       .find({ _id: { $in: prodListObjectId } })
       .toArray();
-      res.send(cartProducts);
+    res.send(cartProducts);
   } catch (err) {
     console.log(err);
   }
-  
 }
--- src/schemas/cart.schemas.js
+++ src/schemas/cart.schemas.js
@@ -1,6 +1,5 @@
 import joi from "joi";
 
 export const cartProductSchema = joi.object({
-    userId:joi.string().hex().length(24).required(),
     productId:joi.string().hex().length(24).required()
 });
